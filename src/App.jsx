68d234367a4513dbebfca041a73ee6ed9a1d4b41
Jacob Brown <jacobbrown0192@gmail.com>
2019-11-16 14:26:09 -0330

    Changes to the blank main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getActiveDeckList, getCardPositions, getGameResult } from './api-framework/api';
+import { getActiveDeckList, getCardPositions, getExpeditionState } from './api-framework/api';
 
 export default class App extends React.Component {
+  state = {data: {}};
 
   async componentDidMount() {
-    await getCardPositions();
-    let intervalId = setInterval(() => {getCardPositions().then(() => {})}, 10000);
+    this.setState({data: await getExpeditionState()});
+    let intervalId = setInterval(() => {getExpeditionState().then((response) => {this.setState({data: response})})}, 10000);
+    let intervalId2 = setInterval(() => {getCardPositions().then(() => {})}, 10000);
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1>IsActive</h1>
+          <div>{Boolean(this.state.data.IsActive).toString()}</div>
+        </div>
+        <div>
+          <h1>Deck</h1>
+          <div>{this.state.data.Deck}</div>
+        </div>
+        <div>
+          <h1>Picks</h1>
+          <div>{this.state.data.Picks}</div>
+        </div>
+        <div>
+          <h1>Games</h1>
+          <div>{this.state.data.Games}</div>
+        </div>
+        <div>
+          <h1>Losses</h1>
+          <div>{this.state.data.Losses}</div>
+        </div>
+        <div>
+          <h1>Wins</h1>
+          <div>{this.state.data.Wins}</div>
+        </div>
+        <div>
+          <h1>Record</h1>
+          <div>{this.state.data.Record}</div>
+        </div>
+        <div>
+          <h1>State</h1>
+          <div>{this.state.data.State}</div>
+        </div>
       </div>
     );
   }
