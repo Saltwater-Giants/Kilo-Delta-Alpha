f67da9d1fa0614de84437ce44d674755f1c1c426
Jacob Brown <jacobbrown0192@gmail.com>
2019-11-16 18:10:01 -0330

    Making everything look slightly better

--- src/containers/DraftDeck.jsx
+++ src/containers/DraftDeck.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './DraftDeck.css'
 
 export default class DraftDeck extends React.Component {
 
   render() {
     return (
       <div>
-        <h1>Deck</h1>
+        <h1 className="draft-deck-title">Deck</h1>
         <div>{this.props.deck}</div>
       </div>
     )
