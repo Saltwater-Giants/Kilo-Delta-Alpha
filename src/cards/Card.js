ce30c26255aa2cf3039e3db8d2e13cacce8f6910
Jacob Brown <jacobbrown0192@gmail.com>
2019-11-17 15:38:50 -0330

    Shows score and has a static template.

--- src/cards/Card.js
+++ src/cards/Card.js
@@ -1,10 +1,11 @@
 const data = require('../ext/datadragon-set1-lite/en_us/data/set1-en_us');
+const scoreTemplate = require('../ext/card-score-tempalate');
 
 export default class Card {
   constructor(id) {
     this.id = id;
     this.info = Card.findCard(id);
-    this.score = 0;
+    this.score = Card.findStaticScore(id);
   }
 
   static findCard(id) {
@@ -12,5 +13,12 @@ export default class Card {
       return card.cardCode === id;
     });
     return cardInfo;
+  }
+
+  static findStaticScore(id) {
+    const cardScore = scoreTemplate.find(card => {
+      return card.id === id;
+    });
+    return cardScore.score;
   }
 }
--- src/components/CardPicks.jsx
+++ src/components/CardPicks.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Card } from './Card';
 import './CardPicks.css'
+import CardPick from '../cards/CardPick';
 
 export const CardPicks = (props) => {
+  let cardPick = new CardPick(props.cards);
+
   return (
     <div className="card-picks">
-      <h2>Pick {props.number + 1}</h2>
+      <h2>Pick {props.number + 1}: {cardPick.score}</h2>
       <div className="card-picks-cards">
-        {props.cards.map((item, i) => (
+        {cardPick.cards.map((item, i) => (
           <Card key={i} {...item}/>
         ))}
       </div>
